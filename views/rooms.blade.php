7257c0ef14e27f7624e7543f5a3a53233bd18e5d
mcpicornell <marc.castro.picornell@gmail.com>
2023-06-23 04:00:29 +0200

    about section styles changes, room.php added an if to show just rooms availabilities

--- views/rooms.blade.php
+++ views/rooms.blade.php
@@ -12,56 +12,55 @@
 
 @if (!empty($rooms))
 <section class="section-selection-rooms">
-
     @foreach ($rooms as $room)
+        @if ($room->isAvailable == true)
+            <div class='section-selection-rooms__rooms'>
+            @if (!empty($room->photos))
+                    @php
+                        $photos = json_decode($room->photos, true);
+                    @endphp
+                    @if (is_array($photos) && count($photos) > 0)
+                    <a href="roomDetails.php?id={{ $room->roomId }}">
+                        <img class="rooms__img" src="{{ $photos[0] }}" />
+                    </a>
+                    @endif
+                @endif
 
-    <div class='section-selection-rooms__rooms'>
-    @if (!empty($room->photos))
-            @php
-                $photos = json_decode($room->photos, true);
-            @endphp
-            @if (is_array($photos) && count($photos) > 0)
-            <a href="roomDetails.php?id={{ $room->roomId }}">
-                <img class="rooms__img" src="{{ $photos[0] }}" />
-            </a>
-            @endif
+                <div class="rooms section-hand-picked__rectangle">
+                    <div class="rectangle__img">
+                        <img class="img rectangle__bed" src="/../img/handPickedRooms/1.bed.svg" />
+                    </div>
+                    <div class="rooms rectangle__img">
+                        <img class="img rectangle__wifi" src="/../img/handPickedRooms/2.wifi.svg" />
+                    </div>
+                    <div class="rooms rectangle__img">
+                        <img class="img rectangle__car" src="/../img/handPickedRooms/3.car.svg" />
+                    </div>
+                    <div class="rooms rectangle__img">
+                        <img class="img rectangle__snow" src="/../img/handPickedRooms/4.snow.svg" />
+                    </div>
+                    <div class="rooms rectangle__img">
+                        <img class="img rectangle__gym" src="/../img/handPickedRooms/5.gym.svg" />
+                    </div>
+                    <div class="rooms rectangle__img">
+                        <img class="img rectangle__noSmoke" src="/../img/handPickedRooms/6.noSmoke.svg" />
+                    </div>
+                    <div class="rooms rectangle__img">
+                        <img class="img rectangle__coctel" src="/../img/handPickedRooms/7.coctel.svg" />
+                    </div>
+                </div>
+
+                <div class="rooms__details">
+                    <h4 class="details__title">{{ $room->roomName }}</h4>
+                    <p class="details__text">Lorem ipsum dolor sit amet, consectetur adipi sicing elit, sed do eiusmod
+                        tempor.</p>
+                    <div class="details__price-room-option">
+                        <span class="price-room-oprion__price">${{ $room->price }}/Night</span>
+                        <span class="price-room-oprion__option">Booking Now</span>
+                    </div>
+                </div>
+            </div>            
         @endif
-
-        <div class="rooms section-hand-picked__rectangle">
-            <div class="rectangle__img">
-                <img class="img rectangle__bed" src="/../img/handPickedRooms/1.bed.svg" />
-            </div>
-            <div class="rooms rectangle__img">
-                <img class="img rectangle__wifi" src="/../img/handPickedRooms/2.wifi.svg" />
-            </div>
-            <div class="rooms rectangle__img">
-                <img class="img rectangle__car" src="/../img/handPickedRooms/3.car.svg" />
-            </div>
-            <div class="rooms rectangle__img">
-                <img class="img rectangle__snow" src="/../img/handPickedRooms/4.snow.svg" />
-            </div>
-            <div class="rooms rectangle__img">
-                <img class="img rectangle__gym" src="/../img/handPickedRooms/5.gym.svg" />
-            </div>
-            <div class="rooms rectangle__img">
-                <img class="img rectangle__noSmoke" src="/../img/handPickedRooms/6.noSmoke.svg" />
-            </div>
-            <div class="rooms rectangle__img">
-                <img class="img rectangle__coctel" src="/../img/handPickedRooms/7.coctel.svg" />
-            </div>
-        </div>
-
-        <div class="rooms__details">
-            <h4 class="details__title">{{ $room->roomName }}</h4>
-            <p class="details__text">Lorem ipsum dolor sit amet, consectetur adipi sicing elit, sed do eiusmod
-                tempor.</p>
-            <div class="details__price-room-option">
-                <span class="price-room-oprion__price">${{ $room->price }}/Night</span>
-                <span class="price-room-oprion__option">Booking Now</span>
-            </div>
-        </div>
-    </div>
-
     @endforeach
 
 
